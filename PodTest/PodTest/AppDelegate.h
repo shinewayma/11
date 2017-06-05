9072361f47bf5b9ba2b48cddb1f80dfdfea283c7
shinewayma <http://hg.hnxinshan.com/sw_ios>
2017-06-05 16:19:02 +0800

    11

---
+++ PodTest/PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by MaXW on 2017/6/2.
+//  Copyright © 2017年 xinshanit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by MaXW on 2017/6/2.
+//  Copyright © 2017年 xinshanit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
